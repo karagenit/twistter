fe8cc435a8dba885451c542ff46d62a3d2a11523
Caleb Smith <karagenit@outlook.com>
2019-09-17 16:40:27 -0400

    Add User Model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname   = models.CharField(max_length=50)
+    lastname    = models.CharField(max_length=50)
+    username    = models.CharField(max_length=50)
+    email       = models.EmailField()
+    password    = models.CharField(max_length=50)
+    birthday    = models.DateField()
+    biography   = models.TextField()
+    profile_pic = models.ImageField(upload_to="uploads/")
