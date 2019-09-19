f6f8a1f501446701e10b9c7bca89a1e33d3e3e0b
Caleb Smith <karagenit@outlook.com>
2019-09-19 13:49:36 -0400

    Update Models

--- blogs/models.py
+++ blogs/models.py
@@ -10,23 +10,35 @@ class User(models.Model):
     biography   = models.TextField(blank=True)
     profile_pic = models.ImageField(blank=True, upload_to="uploads/")
 
+###
+# TODO: assert that every post has 1+ tags (includes empty tag)
+#
 class Post(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_creator")
     likers  = models.ManyToManyField(User, related_name="post_liker")
     content = models.TextField()
     created = models.DateField(auto_now_add=True)
 
+###
+# Note: the blank tag will exist and is a unique tag.
+# Users may not add a blank tag to their posts - the blank tag is
+# added automatically when a post is created without other tags.
+#
 class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True, blank=True)
     posts = models.ManyToManyField(Post)
 
+###
+# Note: if 'tags' is empty, this means the user follows *all* posts by that user
+# this differs from just following all of the user's tags, as it includes future tags too.
+# Users can follow the 'empty' special tag to see posts that have no tags.
+#
+# TODO: allow users to follow just tags without a person?
+#
+# User's timelines consist of all posts by posters they follow that are tagged with at
+# least one of the tags that user follower for that poster.
+#
 class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
-    # if tags is empty, this means the user follows *all* tags by that user
     tags = models.ManyToManyField(Tag)
-    # TODO: if we allow posts with no tags, we need a way to allow a user to follow
-    # tagless posts - since tags can't be empty in name, we have another field
-    # Alternatively, we could allow empty tags, and set that if no other tags present.
-    #follow_empty = models.BooleanField()
-    # TODO: allow actions for newly created tags (e.g. auto follow new tags y/n)
