978cb2231d8337f085ab9a397b073ca9947bbe25
Caleb Smith <karagenit@outlook.com>
2019-09-29 14:46:15 -0400

    Save Registration Form Values in DB

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
+from .models import User
+
+import hashlib
 
 class LoginDash(TemplateView):
     template_name = "log-in.html"
@@ -16,15 +19,25 @@ class SettingsPageView(TemplateView):
 class ProfilePageView(TemplateView):
     template_name = "userprofilepage.html"
 
+def encrypt_string(string):
+    return hashlib.sha256(string.encode()).hexdigest()
+
 def register_user(request):
     # TODO: reject GET requests
 
     # TODO register user
     name = request.POST.get('nameinput', None)
+    firstname, lastname = name.split()
     username = request.POST.get('usernameinput', None)
     email = request.POST.get('emailinput', None)
     password = request.POST.get('passwordinput', None)
     password_confirm = request.POST.get('confirmpasswordinput', None)
+    enc_password = encrypt_string(password)
+    
+    user = User(firstname=firstname, lastname=lastname, username=username, email=email,
+                password=enc_password)
+
+    user.save()
 
     # TODO validate all fields exist & are valid
 
