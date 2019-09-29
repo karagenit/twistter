ba62823c22255a8bc1c87056ffc53c02424870a2
Caleb Smith <karagenit@outlook.com>
2019-09-29 14:21:27 -0400

    Fix Registration Form Fields
    Django requires form input fields to have a name & id attribute

--- blogs/views.py
+++ blogs/views.py
@@ -17,7 +17,16 @@ class ProfilePageView(TemplateView):
     template_name = "userprofilepage.html"
 
 def register_user(request):
+    # TODO: reject GET requests
+
     # TODO register user
+    name = request.POST.get('nameinput', None)
+    username = request.POST.get('usernameinput', None)
+    email = request.POST.get('emailinput', None)
+    password = request.POST.get('passwordinput', None)
+    password_confirm = request.POST.get('confirmpasswordinput', None)
+
+    # TODO validate all fields exist & are valid
 
     # TODO redirect to user's homepage
-    return HttpResponse("User Registered!")
+    return HttpResponse("User Registered! Name: " + name)
