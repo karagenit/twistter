bc73b26c190caf6c4210faa7e9759dab022bbaa0
Thomas Shera <thomasshera@yahoo.com>
2019-09-29 12:47:01 -0400

    Got push button directing to page loginReal

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.urls import path
 from .views import LoginPageView
+from .views import LoginRealPageView
 from django.conf.urls.static import static
 
 urlpatterns = [
     # ... the rest of your URLconf goes here ...
     path('', LoginPageView.as_view(), name='login'),
+    path('loginReal', LoginRealPageView.as_view(), name='loginReal'),
 ]
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,7 @@
 from django.views.generic.base import TemplateView
 
+class LoginRealPageView(TemplateView):
+    template_name = "loginReal.html"
+
 class LoginPageView(TemplateView):
     template_name = "login.html"
