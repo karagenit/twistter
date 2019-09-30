86acc073da732bbee422560062c06a74eacc304a
Thomas Shera <thomasshera@yahoo.com>
2019-09-30 18:12:06 -0400

    Link from toolbar to new post now works

--- blogs/urls.py
+++ blogs/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('mainPage', MainPageView.as_view(), name='mainpage'),
     path('settingsPage', SettingsPageView.as_view(), name='settingspage'),
+    path('makePost', MakePostView.as_view(), name='makepostpage'),
     path('users/<int:pk>', ProfilePageView.as_view(), name='userprofilepage'),
     path('login', login_user),
     path('post', MakePostView.as_view(), name='makepostpage')
