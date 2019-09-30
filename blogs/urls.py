da75d4a2396bad97e3eba0e9a92448a711cc0f0e
haycon0 <hayc@purdue.edu>
2019-09-29 22:16:53 -0400

    Update urls.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path
-from .views import RegisterView, LoginView, MainPageView, ProfilePageView, SettingsPageView, MakePostView
+from .views import RegisterView, LoginView, MainPageView, ProfilePageView, SettingsPageView, login_user, MakePostView
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('mainPage', MainPageView.as_view(), name='mainpage'),
     path('settingsPage', SettingsPageView.as_view(), name='settingspage'),
     path('userProfilePage', ProfilePageView.as_view(), name='userprofilepage'),
+    path('login', login_user),
     path('post', MakePostView.as_view(), name='makepostpage')
 ]
