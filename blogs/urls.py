d595272a5b5a247d2f63eaf5faaa3e526437f95f
Caleb Smith <karagenit@outlook.com>
2019-10-15 14:20:11 -0400

    Add Report Action

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path
-from .views import RegisterView, LoginView, MainPageView, ProfilePageView, SettingsPageView, login_user, MakePostView, SearchView, logout_user, FriendView
+from .views import RegisterView, LoginView, MainPageView, ProfilePageView, SettingsPageView, login_user, MakePostView, SearchView, logout_user, FriendView, report_post
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -16,5 +16,6 @@ urlpatterns = [
     path('post', MakePostView.as_view(), name='makepostpage'),
     path('search', SearchView.as_view(), name='searchpage'),
     path('logout', logout_user),
-    path('friend', FriendView.as_view(), name='friend_page')
+    path('friend', FriendView.as_view(), name='friend_page'),
+    path('report/<int:pk>', report_post, name='report'),
 ]
