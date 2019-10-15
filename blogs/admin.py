a53eafc8da00a02037f953a3f8f48d059b835213
Caleb Smith <karagenit@outlook.com>
2019-10-15 14:16:59 -0400

    Add Reports to Admin Panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Post, Tag, Follow
+from .models import User, Post, Tag, Follow, Report
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Follow)
+admin.site.register(Report)
